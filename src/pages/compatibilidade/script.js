dd2a98fd266e8d6f0112419f457c235a94fbb9d9
Vinicuunha <86085823+Vinicuunha@users.noreply.github.com>
2025-11-13 15:34:41 -0300

    adição da verificação de compatibilidade

---
+++ src/pages/compatibilidade/script.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('formulario_dados');
+const resultado_analise = document.getElementById('resultado_analise');
+
+form.addEventListener('submit',(event) =>{
+    event.preventDefault();
+
+    const dados_preenchido = new FormData(form);
+
+    const valores = Object.fromEntries(dados_preenchido.entries());
+
+    console.log(valores);
+
+    if(valores.jogo_desejado === "gta6"){
+        resultado_analise.innerHTML = '<img id="check" src="/src/imagens/check.png" alt="" width="60px">';
+        resultado_analise.innerHTML = resultado_analise.innerHTML + '<span style="color:#4caf50;">O jogo roda no seu computador</span>';
+    }else{
+        resultado_analise.innerHTML = '<img id="check" src="/src/imagens/cancelar.png" alt="" width="60px">';
+        resultado_analise.innerHTML = resultado_analise.innerHTML + '<span style="color:#fc202a;">O jogo não roda no seu computador</span>';
+
+    }
+})
