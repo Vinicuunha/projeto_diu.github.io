902929743065f60c952512e0b980aff93bacd0e6
Vinicuunha <86085823+Vinicuunha@users.noreply.github.com>
2025-11-25 15:26:09 -0300

    commit

--- src/pages/rick_and_morty.js
+++ src/pages/rick_and_morty.js
@@ -1,6 +1,20 @@
 const retorno = document.getElementById('retorno')
 const URL = 'https://rickandmortyapi.com/api/character'; //url da api
 
+function mostrarPortal(){
+    //mostra a imagem
+    retorno.innerHTML = ``;
+    imagemPortal.style.display = "block"
+
+    //espera 3 segundos
+    setTimeout(() => {
+        imagemPortal.style.display = "none";
+        retorno.style.display = "block"
+        chamarApi();
+    }, 500)
+}
+
+
 async function chamarApi() { //funcao da api
     const resposta = await fetch(URL); // const pra esperar a api processar as requisições
     console.log(resposta) //imprime o retorno no console
@@ -10,7 +24,6 @@ async function chamarApi() { //funcao da api
         console.log(sorteador)
         for (let i = 0; i < sorteador; i++) {
             if (sorteador === objeto.results[i].id) {
-                console.log("achou")
                 retorno.innerHTML = `<img src="${objeto.results[i].image}"><br>`;
                 retorno.innerHTML += `Nome: ${objeto.results[i].name}<br>`;
                 retorno.innerHTML += `Espécie: ${objeto.results[i].species}<br>`;
@@ -21,4 +34,3 @@ async function chamarApi() { //funcao da api
     }
 }
 
-chamarApi();
